57db27f013df7a4b970a9986c70bad868e25dc52
antoniohps26 <antonio.hps26@gmail.com>
2021-06-10 19:37:53 -0300

    Terceiro Commit - Encontros 8720 a 8732

--- scripts/unsc_2020_script.R
+++ scripts/unsc_2020_script.R
@@ -1,7 +1,5 @@
 ### unsc extract
-library(textreadr)
 library(tidyverse)
-library(pdftools)
 library(magick)
 library(tesseract)
 library(lubridate)
@@ -9,7 +7,7 @@ library(quanteda)
 library(stringi)
 
 eng <- tesseract(language = "eng", options = list(tessedit_pageseg_mode = 1))
-readpdf <- image_read_pdf("speeches_pdf/S_PV.8720_E.pdf")
+readpdf <- image_read_pdf("speeches_pdf/S_PV.8732_E.pdf")
 txt <- ocr(readpdf, engine = eng)
 readpdf2 <- str_replace_all(paste(txt, collapse= " "), "[:blank:]{2,}","") 
 readpdf3 <- str_replace_all(readpdf2, "\\\n"," ")
@@ -19,7 +17,7 @@ readpdf3 <- stri_trans_general(readpdf3, "Latin-ASCII")
 # vector speech
 
 separate_unsc <- str_split(readpdf3,
-                           "(?=\\b(Dato[ ]Lim[ ]Jock[ ]Hoi[:]))|(?=\\b(Monsignor[ ]Hansen|The[ ]President|President[ ]Abbas|Aboul[ ]Gheit)[ ][()])|(?=\\b(The President|The Secretary-General)\\:)|(?=\\b(Mr|Ms|Mrs)[.][ ](Lowcock|Griffiths|Chambas|Robinson|Skoog|Mardini|Ruiz[ ]Massieu|Lacroix|Rajasingham|DiCarlo|Mueller|Raz|Matar|Salame|Licharz|Nakamitsu|Spleeters|Rama|Pedersen|Voronkov|Coninsx|Freij|Mladenov)[:])|(?=\\b(Mr|Mrs|Ms)[.][ ]([A-Z]*[a-z]*|[A-Z]*[a-z]*[ ][A-Z]*[a-z]*|[A-Z]*[a-z]*[ ][A-Z]*[a-z]*[ ][A-Z]*[a-z]*|[A-Z]*[a-z]*[-][A-Z]*[a-z]*|[A-Z]*[a-z]*[A-Z]*[a-z]*|[A-Z]*[a-z]*.[A-Z]*[a-z]*)[ ][(])") %>% unlist()
+                           "(?=\\b(Dato[ ]Lim[ ]Jock[ ]Hoi[:]))|(?=\\b(Monsignor[ ]Hansen|The[ ]President|President[ ]Abbas|Aboul[ ]Gheit|King[ ]Philippe|The Secretary-General)[ ][()])|(?=\\b(The President|The Secretary-General)\\:)|(?=\\b(Mr|Ms|Mrs)[.][ ](Lowcock|Griffiths|Chambas|Robinson|Skoog|Mardini|Ruiz[ ]Massieu|Lacroix|Rajasingham|DiCarlo|Mueller|Raz|Matar|Salame|Licharz|Nakamitsu|Spleeters|Rama|Pedersen|Voronkov|Coninsx|Freij|Mladenov|Becker|Bachelet|De[ ]Roux|Sooka|Sori[-]Coulibaly|Costa[ ]Filho|Grau|Cevik|La[ ]Lime|Swan|Madeira|Smith)[:])|(?=\\b(Mr|Mrs|Ms)[.][ ]([A-Z]*[a-z]*|[A-Z]*[a-z]*[ ][A-Z]*[a-z]*|[A-Z]*[a-z]*[ ][A-Z]*[a-z]*[ ][A-Z]*[a-z]*|[A-Z]*[a-z]*[-][A-Z]*[a-z]*|[A-Z]*[a-z]*[A-Z]*[a-z]*|[A-Z]*[a-z]*.[A-Z]*[a-z]*|Pinto[ ]Lopes[ ]D.Alva)[ ][(])") %>% unlist()
 
 # tibble speech
 
@@ -67,7 +65,7 @@ df$speaker <- sub("[ ][(]", "", df$speaker)
 
 # participant type part 1
 
-df$participanttype <- str_extract(df$content, "(Monsignor[ ]Hansen|Dato[ ]Lim[ ]Jock[ ]Hoi|President[ ]Abbas|Aboul[ ]Gheit)|(The President|The Secretary-General)|((Mr|Ms|Mrs)[.][ ](Lowcock|Griffiths|Chambas|Robinson|Skoog|Mardini|Ruiz[ ]Massieu|Lacroix|Rajasingham|DiCarlo|Mueller|Raz|Matar|Salame|Licharz|Nakamitsu|Spleeters|Rama|Pedersen|Voronkov|Coninsx|Freij|Mladenov))|((Mr|Mrs|Ms)[.][ ]([A-Z]*[a-z]*|[A-Z]*[a-z]*[ ][A-Z]*[a-z]*|[A-Z]*[a-z]*[ ][A-Z]*[a-z]*[ ][A-Z]*[a-z]*|[A-Z]*[a-z]*[-][A-Z]*[a-z]*|[A-Z]*[a-z]*[A-Z]*[a-z]*|[A-Z]*[a-z]*.[A-Z]*[a-z]*)[ ][()])")
+df$participanttype <- str_extract(df$content, "(Monsignor[ ]Hansen|Dato[ ]Lim[ ]Jock[ ]Hoi|President[ ]Abbas|Aboul[ ]Gheit|King[ ]Philippe)|(The President|The Secretary-General)|((Mr|Ms|Mrs)[.][ ](Lowcock|Griffiths|Chambas|Robinson|Skoog|Mardini|Ruiz[ ]Massieu|Lacroix|Rajasingham|DiCarlo|Mueller|Raz|Matar|Salame|Licharz|Nakamitsu|Spleeters|Rama|Pedersen|Voronkov|Coninsx|Freij|Mladenov|Becker|Bachelet|De[ ]Roux|Sooka|Sori[-]Coulibaly|Costa[ ]Filho|Grau|Cevik|La[ ]Lime|Swan|Madeira|Smith))|((Mr|Mrs|Ms)[.][ ]([A-Z]*[a-z]*|[A-Z]*[a-z]*[ ][A-Z]*[a-z]*|[A-Z]*[a-z]*[ ][A-Z]*[a-z]*[ ][A-Z]*[a-z]*|[A-Z]*[a-z]*[-][A-Z]*[a-z]*|[A-Z]*[a-z]*[A-Z]*[a-z]*|[A-Z]*[a-z]*.[A-Z]*[a-z]*|Pinto[ ]Lopes[ ]D.Alva)[ ][()])")
 
 df$participanttype <- sub("[ ][(]", "", df$participanttype)
 
@@ -75,7 +73,7 @@ df$participanttype <- sub("[ ][(]", "", df$participanttype)
 
 df$speaker <- str_trim(df$speaker)
 
-df$speaker[df$speaker == "The President"] <- 'Mr. Pecsteen de Buytswerve'
+df$speaker[df$speaker %in% c("The President", "Mr. President")] <- 'Mr. Pecsteen de Buytswerve'
 
 # speech number
 
@@ -90,8 +88,12 @@ df$country[df$participanttype == "The President"] <- 'Belgium'
 df$country[df$speaker %in% c("Mr. Chambas", "Mrs. Robinson", "The Secretary-General", "Ruiz Massieu", "Lacroix", "Mr. Griffiths",
                              "Mr. Rajasingham", "Ms. DiCarlo", "Ms. Mueller", "Mr. Lowcock", "Ms. Matar", "Mr. Salame", "Mr. Licharz",
                              "Mrs. Nakamitsu", "Mr. Spleeters", "Mr. Pedersen", "Mr. Voronkov", "Ms. Coninsx",
-                             "Mr. Mladenov")] <- 'UN'
+                             "Mr. Mladenov", "Ms. Bachelet", "Ms. Sori-Coulibaly", "Mr. Ndiaye", "Mr. Hilale", "Ms. La Lime",
+                             "Mr. Swan")] <- 'UN'
 
+# Maybe also representative
+df$country[df$speaker %in% c("Mr. Costa Filho")] <- 'UN'
+#
 df$country[df$speaker %in% c("Mr. Mardini")] <- 'International Committee of the Red Cross'
 
 df$country[df$speaker %in% c("Mrs. Raz")] <- 'Committee on the Exercise of the Inalienable Rights of the Palestinian People'
@@ -102,13 +104,29 @@ df$country[df$speaker %in% c("Mr. Abdelaziz", "Mr. Aboul Gheit")] <- 'League of 
 
 df$country[df$speaker %in% c("Dato Lim Jock Hoi")] <- 'ASEAN'
 
-df$country[df$speaker %in% c("Mr. Rama")] <- 'OSCE'
+df$country[df$speaker %in% c("Mr. Rama", "Ms. Grau", "Mr. Cevik")] <- 'OSCE'
 
 df$country[df$speaker %in% c("Ms. Freij")] <- 'Individual'
 
 df$country[df$speaker %in% c("President Abbas")] <- 'Palestine'
 
+df$country[df$speaker %in% c("Ms. Becker")] <- 'Watchlist on Children and Armed Conflict'
+
+df$country[df$speaker %in% c("Mr. Chergui", "Mr. Matondo", "Mr. Madeira")] <- 'African Union'
+
+df$country[df$speaker %in% c("King Philippe")] <- 'Belgium'
+
+df$country[df$speaker %in% c("Mr. De Roux")] <- 'Commission for the Clarification of Truth, Coexistence and Non-Repetition of Colombia'
+
+df$country[df$speaker %in% c("Ms. Sooka")] <- 'Foundation for Human Rights in South Africa'
+
+df$country[df$speaker %in% c("Ms. Gilles")] <- 'Fondasyon Je Klere (FJKL)'
+
+df$country[df$speaker %in% c("Mr. Smith")] <- 'Stockholm International Peace Research Institute'
+
 # participant type part 2
+
+df$participanttype[df$speaker %in% c("Mr. President")] <- 'The President'
 
 df$participanttype[df$country %in% c("Viet Nam",
                                      "China",
@@ -177,11 +195,13 @@ df$sentences <- nsentence(df$content)
 
 df$content <- gsub("[ ][ ][0-9]{1,2}[/][0-9]{1,2}[ ][ ]", "", df$content)
 
-df$content <- gsub("[ ][ ]20-03535[ ][ ]", "", df$content)
+df$content <- gsub("[ ][ ]20-05035[ ][ ]", "", df$content)
 
-df$content <- gsub("S[/]PV[.]8718  Reports of the Secretary-General on the Sudan and South Sudan  11[/]02[/]2020", "", df$content)
+#df$content <- gsub("[(]Resumption[ ]1[)][ ]", "", df$content)
 
-df$content <- gsub("11[/]02[/]2020  Reports of the Secretary-General on the Sudan and South Sudan  S[/]PV[.]8718", "", df$content)
+df$content <- gsub("S[/]PV[.]8732  The situation in the Middle East  25[/]02[/]2020", "", df$content)
+
+df$content <- gsub("25[/]02[/]2020  The situation in the Middle East  S[/]PV[.]8732", "", df$content)
 
 # teste para footer
 
